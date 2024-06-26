2af0b6ceecbba86ef0c9af8d393fdf7f259d36a6
Tuzbay <t.uzbayyelce@gmail.com>
2024-06-26 18:27:24 +0300

    Single Page & Slider

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './Slider.scss';
+
+const Slider = ({ images }) => {
+  const [imageIndex, setImageIndex] = useState(null);
+
+  const changeSlide = (direction) => {
+    if (direction === 'left') {
+      if (imageIndex === 0) {
+        setImageIndex(images.length - 1);
+      } else {
+        setImageIndex(imageIndex - 1);
+      }
+    } else {
+      if (imageIndex === images.length - 1) {
+        setImageIndex(0);
+      } else {
+        setImageIndex(imageIndex + 1);
+      }
+    }
+  };
+
+  return (
+    <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow" onClick={() => changeSlide('left')}>
+            <img src="/arrow.png" alt="Arrow" />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow" onClick={() => changeSlide('right')}>
+            <img className="right" src="/arrow.png" alt="Arrow" />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            X
+          </div>
+        </div>
+      )}
+
+      <div className="bigImage">
+        <img
+          src={images[0]}
+          alt="Big Images"
+          onClick={() => setImageIndex(0)}
+        />
+      </div>
+      <div className="smallImages">
+        {images.slice(1).map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt="Small Image"
+            onClick={() => setImageIndex(index + 1)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,8 +1,117 @@
-import React from 'react';
 import './SinglePage.scss';
+import Slider from '../../components/Slider/Slider';
+import { singlePostData, userData } from '../../lib/dummyData';
+import Map from '../../components/Map/Map';
 
 const SinglePage = () => {
-  return <div>SinglePage</div>;
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData?.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData?.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData?.address}</span>
+                </div>
+                <div className="price">${singlePostData?.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData?.img} alt="" />
+                <span>{userData?.name}</span>
+              </div>
+            </div>
+
+            <div className="bottom">{singlePostData?.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper">
+          <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="Utility Icon" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is responsible</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="Utility Icon" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/utility.png" alt="Utility Icon" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Sizes</p>
+          <div className="sizes">
+            <div className="size">
+              <img src="/size.png" alt="Size Icon" />
+              <span>80sqft</span>
+            </div>
+            <div className="size">
+              <img src="/bed.png" alt="Size Icon" />
+              <span>2 beds</span>
+            </div>
+            <div className="size">
+              <img src="/bath.png" alt="Size Icon" />
+              <span>1 bathroom</span>
+            </div>
+          </div>
+          <p className="title">Nearby Places</p>
+          <div className="listHorizontal">
+            <div className="feature">
+              <img src="/school.png" alt="School Icon" />
+              <div className="featureText">
+                <span>School</span>
+                <p>250m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="Bus Icon" />
+              <div className="featureText">
+                <span>Bus Stop</span>
+                <p>100m away</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="Restaurant Icon" />
+              <div className="featureText">
+                <span>Restaurant</span>
+                <p>200m away</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Location </p>
+          <div className="mapContainer">
+            <Map items={[singlePostData]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="Chat Icon" />
+              Send a Message
+            </button>
+            <button>
+              <img src="/save.png" alt="Chat Icon" />
+              Save the Place
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
